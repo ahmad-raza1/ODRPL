778a329d77efe03b2069d92160cd2730f8e97110
Ahmad Raza <ahmadraza_@outlook.com>
2021-06-26 02:37:37 +0500

    Added search input, list abstracts with for-loop

--- mysite/ODRPL/views.py
+++ mysite/ODRPL/views.py
@@ -1,12 +1,28 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.views import View
 
 def index(request):
 	return render(request, 'index.html')
 
+
 def abstracts(request):
-	return render(request, 'abstracts.html')
+
+	if request.method == 'POST':
+		query_text = str(request.POST['InputSubject'])
+
+		# clearing extra/duplicate whitespaces
+		query_text = " ".join(query_text.split())
+
+		context = {
+			"query_text": query_text,
+			"size": 10
+		}
+
+		return render(request, 'abstracts.html', context)
+
 
 def loader(request):
 	return render(request, 'loader.html')
