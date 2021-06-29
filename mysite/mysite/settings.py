c6b13ea81a7602df88d9fc6c376d91b0595d38ca
Ahmad Raza <ahmadraza_@outlook.com>
2021-06-29 23:30:42 +0500

    Changed settings.py for collectstatic

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,10 +136,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ODRPL/static'),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ODRPL\static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
 
 #STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
