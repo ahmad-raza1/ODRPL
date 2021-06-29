c6cd15abc4c00c89fca63e15dafed7eff1501894
Ahmad Raza <ahmadraza_@outlook.com>
2021-06-30 01:48:50 +0500

    Changed wsgi.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
+"""
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+"""
 application = get_wsgi_application()
